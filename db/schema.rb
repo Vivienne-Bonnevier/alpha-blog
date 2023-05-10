1442614f42fe6074ea77f11f04ba34d99dcb9670
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-10 12:00:31 -0500

    Add password_digest column to users table for creating a password hash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_160427) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_164336) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_160427) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
