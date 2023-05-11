29bb496d736e01f51996d92348e82407060320d4
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-11 10:09:35 -0500

    Fix the articles controller so that articles are authored by currently logged in user. Add redirect from home to articles for logged in users. Move authentication methods from helpers to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # with the way that Ruby handles true and false (truthy and falsey), isn't this code redundant? Calling current_user would have the same effect
+  def logged_in?
+    !!current_user
+  end
+
 end
