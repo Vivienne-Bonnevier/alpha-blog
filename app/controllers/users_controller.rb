a473b3a6f234e647626bf52ef1d0d42adbdd097e
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-11 11:42:06 -0500

    Allow the deletion of users and associated articles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
     # this MUST come before
-    before_action :require_user, except: [:show, :index]
+    before_action :require_user, only: [:edit, :update, :destroy]
     before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome, #{@user.username}, to the Alpha Blog."
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all authored articles deleted"
+    redirect_to root_path
   end
 
   private
