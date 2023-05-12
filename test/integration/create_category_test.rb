e9d9b018210013666fc5712072273f94be8fef45
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-12 09:36:00 -0500

    Create further tests and padginate the categories index

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  test "get the new category form and create the category" do
+    get "/categories/new"
+    assert_response :success
+    assert_difference "Category.count", 1 do
+      post categories_path, params: { category: { name: "Sports" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+
+  test "get the new category form and reject empty category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "" } }
+    end
+    assert_match "Name can&#39;t be blank", response.body
+  end
+
+  test "get the new category form and reject short category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "Ax" } }
+    end
+    assert_match "Name is too short (minimum is 3 characters)", response.body
+  end
+
+  test "get the new category form and reject long category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "a" * 26 } }
+    end
+    assert_match "Name is too long (maximum is 25 characters)", response.body
+  end
+
+  test "get the new category form and reject duplicate category submission" do
+    @category = Category.create(name: "Sports")
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+      post categories_path, params: { category: { name: "Sports" } }
+    end
+    assert_match "Name has already been taken", response.body
+  end
+
+end
+ 
---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @category1 = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
+  end
+
+  test "show categories listing" do
+    get "/categories"
+    assert_select "a[href=?]", category_path(@category1), text: @category1.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+
+  test "ensure proper pagination of 5 per page" do
+    @category3 = Category.create(name: "Healthcare")
+    @category4 = Category.create(name: "Physical Fitness")
+    @category5 = Category.create(name: "Streaming")
+    @category6 = Category.create(name: "Gaming")
+    get "/categories"
+    # should find all
+    assert_select "a[href=?]", category_path(@category1), text: @category1.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+    assert_select "a[href=?]", category_path(@category3), text: @category3.name
+    assert_select "a[href=?]", category_path(@category4), text: @category4.name
+    assert_select "a[href=?]", category_path(@category5), text: @category5.name
+    # should NOT find (padgination)
+    assert_select "a[href=?]", category_path(@category6), false, text: @category6.name
+  end
+end
