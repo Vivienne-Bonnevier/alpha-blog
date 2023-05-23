7ca0221fa5ee13cd8d10ee9b27ffa3fce34bf0f4
Vivienne Bonnevier <vivibonnevier@gmail.com>
2023-05-23 14:24:06 -0500

    Fix error in GenerateArticleJob

--- app/jobs/generate_article_job.rb
+++ app/jobs/generate_article_job.rb
@@ -9,14 +9,6 @@ class GenerateArticleJob < ApplicationJob
     article.save
     rand(0..5).times do
       category = Category.find(Category.pluck(:id).sample)
-      unless article.categories.where(name: category.name).exists?
-        article.categories << category
-        article.save
-      end
-    end
-  end
-end
-      category = Category.find(Category.pluck(:id).sample)
       unless article.categories.exists?(category.id)
         article.categories << category
         article.save
